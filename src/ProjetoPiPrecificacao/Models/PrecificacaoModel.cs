d4044dace9c32f9acf34e988eb2670b9cb0ae2f4
Andreza Amorim <amrimandreza@gmail.com>
2025-05-13 23:10:44 -0300

    Ajuste query salvar

--- src/ProjetoPiPrecificacao/Models/PrecificacaoModel.cs
+++ src/ProjetoPiPrecificacao/Models/PrecificacaoModel.cs
@@ -4,7 +4,7 @@
     {
         public int? IdPrecoProduto { get; set; }
         public float? PrecoSugeridoSTSP { get; set; }
-        public float PrecoVenda { get; set; }
+        public float? PrecoVenda { get; set; }
         public float? Desconto { get; set; }
         public float? PrecoDesconto { get; set; }
         public float? MargemLiquida { get; set; }
--- src/ProjetoPiPrecificacao/Repository/Queries/PrecificacaoRepositoryQueries.cs
+++ src/ProjetoPiPrecificacao/Repository/Queries/PrecificacaoRepositoryQueries.cs
@@ -15,7 +15,7 @@
                 INSERT INTO public.""dbo.Tabela_Preco_Produto""
                 (""Id_Produto"", ""Preco_Sugerido_STSP"", ""Preco_Venda"", ""Desconto"",
                  ""Preco_Com_Desconto"", ""Margem_Liquida"", ""Margem_Bruta"", ""Valor_Lucro"",
-                 ""Data_Alteracao_Preco)
+                 ""Data_Alteracao_Preco"")
                 VALUES (@IdProduto, @PrecoSugeridoSTSP, @PrecoVenda, @Desconto, @PrecoDesconto,
                  @MargemLiquida, @MargemBruta, @Lucro, @DataAlteracaoPreco)
             ";
