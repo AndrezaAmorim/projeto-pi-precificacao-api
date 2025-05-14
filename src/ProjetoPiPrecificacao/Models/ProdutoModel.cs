3fc304727ff7066b21289d3b7d0e4ed1c190f5a1
Andreza Amorim <amrimandreza@gmail.com>
2025-05-13 22:30:56 -0300

    ajuste de nullable

--- src/ProjetoPiPrecificacao/Models/ProdutoModel.cs
+++ src/ProjetoPiPrecificacao/Models/ProdutoModel.cs
@@ -2,14 +2,14 @@
 {
     public class ProdutoModel
     {
-        public int IdProduto { get; set; }
-        public int IdCustoProduto { get; set; }
+        public int? IdProduto { get; set; }
+        public int? IdCustoProduto { get; set; }
         public string SKU { get; set; }
         public string NomeProduto { get; set; }
         public string Fornecedor { get; set; }
-        public float Peso { get; set; }
-        public float Altura { get; set; }
-        public float Largura { get; set; }
+        public float? Peso { get; set; }
+        public float? Altura { get; set; }
+        public float? Largura { get; set; }
         public bool Kit { get; set; }
         public DateTime DataCompra { get; set; }
         public string TipoCompra { get; set; }
