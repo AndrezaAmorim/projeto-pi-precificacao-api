c598ea693a85646175747e497ef0fd076a77bc4c
Andreza Amorim <54220041+AndrezaAmorim@users.noreply.github.com>
2026-05-08 21:47:28 -0300

    Add Excluir method to IProdutoBusiness interface

--- src/ProjetoPiPrecificacao/Business/Interface/IProdutoBusiness.cs
+++ src/ProjetoPiPrecificacao/Business/Interface/IProdutoBusiness.cs
@@ -5,6 +5,7 @@ namespace ProjetoPiPrecificacao.Business.Interface
     public interface IProdutoBusiness
     {
         bool Cadastrar(ProdutoModel model);
+        bool Excluir(ProdutoModel model);
         ProdutoModel? BuscarProdutoPorSku(string SKU);
         Task CadastrarProdutoExcel(IFormFile arquivo);
     }
